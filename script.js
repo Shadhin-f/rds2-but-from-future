14eaa44acffe1fe42f1d57dff68376e00ab3a5ce
Tahshan Jamil Shadhin <tj.shadhin001@gmail.com>
2025-05-29 16:56:58 +0600

    Final course data update

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function animate() {
 
 animate();
 
-const CSV_FILENAME = 'l76.csv';
+const CSV_FILENAME = 'l77.csv';
 const CACHE_KEY = `courseData_${CSV_FILENAME}`;
 let courseData = [];
 let filteredData = []; // Add this line to store filtered data globally
