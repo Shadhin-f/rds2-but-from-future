8ce14aa0068309077429307a49873fc37d6838b9
Tahshan Jamil Shadhin <tj.shadhin001@gmail.com>
2025-09-13 00:17:36 +0600

    live 7

--- script.js
+++ script.js
@@ -755,7 +755,7 @@ window.addEventListener('load', () => {
 });
 
 // Add these at the top with your other constants
-const DEFAULT_SEMESTER = 'live_6.csv';
+const DEFAULT_SEMESTER = 'live_7.csv';
 let currentSemester = DEFAULT_SEMESTER;
 
 // Add this function to handle semester changes
