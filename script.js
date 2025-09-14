7c5244e995df7729478beb173f58f2275c15eab8
Tahshan Jamil Shadhin <tj.shadhin001@gmail.com>
2025-09-15 05:27:00 +0600

    live 20 - 15 sept.

--- script.js
+++ script.js
@@ -755,7 +755,7 @@ window.addEventListener('load', () => {
 });
 
 // Add these at the top with your other constants
-const DEFAULT_SEMESTER = 'live_19.csv';
+const DEFAULT_SEMESTER = 'live_20.csv';
 let currentSemester = DEFAULT_SEMESTER;
 
 // Add this function to handle semester changes
