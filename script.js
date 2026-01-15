0e28d79c3a9b157d75b2a2aafb9f8e7deb77fd12
Tahshan Jamil Shadhin <tj.shadhin001@gmail.com>
2026-01-15 21:24:40 +0600

    update

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ function animate() {
 animate();
 
 // ========== CHANGE THIS VALUE TO UPDATE DEFAULT SEMESTER ==========
-const DEFAULT_SEMESTER = '261_v12.csv';
+const DEFAULT_SEMESTER = '261_v13.csv';
 // ===================================================================
 
 let CSV_FILENAME = DEFAULT_SEMESTER;
